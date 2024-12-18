be99b995bd548b251255ba6831239926900fee9e
katia-aa <katia.amir@gmail.com>
2024-12-18 12:21:02 -0500

    Fix serving assets error

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/mutts-waver-form-with-raffle/", // Replace 'mutts-waver-form-with-raffle' with your repository name
-  envDir: "./", // Ensure this points to the directory containing `.env`
+  base: "./", 
   build: {
     outDir: 'dist', // Output directory for build
   },
